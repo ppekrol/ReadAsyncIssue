dd7d04dfd2c03d058b5cf6f3cf69b6eba242b528
Pawel Pekrol <ppekrol@ais.pl>
2015-05-22 12:45:45 +0200

    initial commit

---
+++ src/ReadAsyncIssue.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ReadAsyncIssue.Console
+{
+	public class Program
+	{
+		private readonly byte[] buffer = new byte[8192];
+
+		public void Main(string[] args)
+		{
+			DoWorkAsync().Wait();
+		}
+
+		private async Task DoWorkAsync()
+		{
+			using (var httpClient = new HttpClient())
+			{
+				var message = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5000/api/values");
+
+				var response = await httpClient
+					.SendAsync(message, HttpCompletionOption.ResponseHeadersRead)
+					.ConfigureAwait(false);
+
+				var stream = await response
+					.Content
+					.ReadAsStreamAsync()
+					.ConfigureAwait(false);
+
+				var read = await stream
+					.ReadAsync(buffer, 0, buffer.Length)
+					.ConfigureAwait(false);
+
+				if (read == buffer.Length)
+					throw new InvalidOperationException();
+			}
+		}
+	}
+}
---
+++ src/ReadAsyncIssue/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+
+namespace ReadAsyncIssue.Controllers
+{
+	public class StreamResult : IActionResult
+	{
+		public Task ExecuteResultAsync(ActionContext context)
+		{
+			context.HttpContext.Response.ContentType = "text/event-stream";
+			var s = new string('t', 100);
+
+
+			while (true)
+			{
+				context.HttpContext.Response.WriteAsync(s);
+
+				Thread.Sleep(100);
+			}
+		}
+	}
+
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+		public IActionResult Get()
+		{
+			return new StreamResult();
+		}
+	}
+}
